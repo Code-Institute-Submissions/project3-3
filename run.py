eadaf4cb8cb25cb3c33f098262beda384d2d5a7b
Piotr Nowakowski <nowakowski.peter@gmail.com>
2018-10-02 19:38:12 +0000

    Added basic login functionality

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 import os
 
 app = Flask(__name__)
+users = { }
 
 @app.route('/')
 
 def index():
     return render_template('base.html')
     
-@app.route('/about')
+@app.route('/', methods = ['POST'])
+def user():
+    print (request.form['Username'])
+    return render_template('base.html', username = request.form['Username'])
 
 def about():
     return 'about us'
