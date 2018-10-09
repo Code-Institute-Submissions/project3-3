3857b4cd70a1877af5e10c09c457974d5c1a14f8
Piotr Nowakowski <nowakowski.peter@gmail.com>
2018-10-09 09:45:18 +0000

    Deleted .git dir by accident, wonder if I lose all commit history...

---
+++ README.md
@@ -0,0 +1,69 @@
+# Riddle Me This
+
+#### [See on Heroku](https://project-3-riddle.herokuapp.com/)
+
+## UX
+
+- As a user I want to play a mind-bending guessing game and possibly compare my achievements against other players
+    - The game features 49 questions and points system that allows players to compete against one another
+- As a user I want to be able to skip questions that prove to be too hard
+    - There's an option for users to skip any question they don't know the answer to. Supplying a wrong answer results in negative points being awarded.
+- As a user I want to have a visually pleasing experience
+    - This one's a bit subjective, but I think the combination of background image, website colors and transparency isn't the worst.
+
+
+## Features
+
+- 49 questions ranging from very easy to being quite a puzzle
+- Persistent Highscores - saved to file and retrieved upon restaring the environment
+- Javascript Free - All you no-script paranoid people can rejoice!
+- Easy to navigate - the game consists of one page really
+
+## Future features
+
+- Possibly implement highscores.pkl pruning after it reaches certain size
+- Ability for active users to chat
+- Make questions random for each started game?
+
+## Technologies Used
+
+- [Flask framework](http://flask.pocoo.org/) - to learn it and make use of the 'views' concept
+
+## Testing
+
+1. Giving out correct answers to incoming questions advances round and awards points as expected
+    - Wrong answers give negative points
+    - Not supplying any answer and pressing submit counts as a wrong answer, as expected
+
+2. Upon answering the last question I was able to start the game over
+3. Upon pressing the 'skip' button I was taken to the next question as expected
+4. [FIXED] Supplying username and pressing 'submit' allows me to start the game
+    - Tried very long usernames and they look unappealing both in the scoreboard and highscores
+        - This will be solved by adding a limit of 15 characters in the username input box 
+5. Tried pressing browser's 'back' button while being on question 3, which took me to the page with question 2, however the game still expects the answer to question 3
+    so technically I'm giving a correct answer to the question I see (and had a shot at before) but the script is expecting answer to question 3 and awards me negative points
+    Don't know the proper solution to this at the moment
+6. [FIXED] Supplying a username that already exists will allow us to play as one, and add to its existing score by answering questions from the beggining
+    - this will be fixed so that users can't choose existing names
+
+## Deployment
+
+1. ```wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh```
+2. Create heroku account
+    - Create new app with name project-3-riddle
+    - next in the console
+     ```heroku login ```
+    - next
+    - ``` heroku git:remote -a project-3-riddle ```
+    - ``` echo web: python run.py > Procfile ```
+    - ```sudo pip3 freeze --local > requirements.txt```
+    - ```git add .```
+    - ```git commit -m "Heroku setup" ```
+    - ``` git push heroku master```
+    - One last thing is going to our app settings on heroku.com and clicking 'reveal config vars'
+    - Add IP in the 'key' field and '0.0.0.0' in the value field. Click add.
+    - In the next set, add PORT and 5000 respectively
+
+## Credits
+
+I received inspiration for this project from CodeInstitute's milestone project outline
---
+++ run.py
@@ -0,0 +1,142 @@
+from flask import Flask, render_template, request, redirect, url_for
+import operator
+import os
+import time
+import pickle
+
+app = Flask(__name__)
+
+# remove passing debug (answer) to render_template on release
+highscores = { }
+
+
+# save_obj and load_obj are used to save highscores for persistency
+
+def save_obj(obj, name ):
+    with open('data/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name ):
+    with open('data/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+if len(highscores) == 0:
+    highscores = load_obj('highscores')
+    
+class Player:
+    @staticmethod
+    def getScores():    # returns a list of tuples with highscores (first 10)
+        return sorted(highscores.items(), reverse = True, key=operator.itemgetter(1))[:10]
+
+                
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+        self.question = 0
+        self.finished = False # will be set to true upon answering last question
+        
+    def addPoints(self, number = 5):
+        self.score += number
+        save_obj(highscores, 'highscores')
+        # since we're adding points, we can as well advance question
+        if self.question >= len(questions)-1:
+            self.finished = True
+        else:
+            self.question += 1
+
+            
+        highscores[self.name] = self.score
+
+        
+    def removePoints(self, number = 1):
+        self.score -= number
+        if self.score < 0:
+            self.score = 0
+        highscores[self.name] = self.score
+        save_obj(highscores, 'highscores')
+        
+    def getQandA(self): # returns ('question', 'answer')
+        return questions[self.question]
+    def getQuestion(self): # returns 'question'
+        return questions[self.question][0] or questions[-1][0]
+    @classmethod
+    def exists(cls, name):
+        return name in users
+    
+questions = [] # ('question', 'answer')
+users = { } # 'name' = Player obj
+
+#
+
+def loadQuestions():
+    count = 0
+    with open('data/questions.txt', 'r') as fp:
+        question = answer = ''
+        for i, line in enumerate(fp):
+            line = line.rstrip()
+            if i % 2 == 0: # it's a question
+                question = line
+                count += 1
+            else:
+                answer = line
+                questions.append( (question, answer) )
+    return count            
+       
+totalQuestions = loadQuestions()
+
+@app.route('/')
+def index():
+    return render_template('base.html', count = "[ 0 / {0} ]".format(totalQuestions),scores = Player.getScores())
+    
+@app.route('/', methods = ['GET', 'POST'])
+def login():
+    if ('Username' in request.form):
+        username = request.form['Username']
+        
+        # create a new entry if the user isn't there already
+        if not Player.exists(username) and not username in highscores:
+            users[username] = Player(username)
+        else:
+            return render_template('base.html', error_name = username, count = "[ 0 / {0} ]".format(totalQuestions),scores = Player.getScores())
+        return redirect(url_for('game', count = "[ 1 / {0} ]".format(totalQuestions), username = username, scores = Player.getScores()))
+        #return redirect(username + '/question')
+
+@app.route('/<username>/question', methods = ['GET', 'POST'])
+def game(username):
+    player = users[username]
+    question, answer = player.getQandA()
+    
+    if ('answer' in request.form):
+        if answer.lower() in request.form['answer'].lower():
+            player.addPoints()
+            if player.finished:
+                return redirect(username + '/finished')
+            
+            return render_template('base.html', time = time.time(), scores = Player.getScores(),  count = "[ {0} / {1} ]".format(player.question+1, totalQuestions), username = username, success = True, question = player.getQuestion(), score = player.score)
+        else:
+            #wrong answer
+            player.removePoints()
+            return render_template('base.html', debug = answer, time = time.time(), scores = Player.getScores(), count = "[ {0} / {1} ]".format(player.question+1, totalQuestions), wrong_answer = request.form['answer'], username = username, success = False, question = question, score = player.score)
+    else:
+        if 'skip_button' in request.form:
+            player.addPoints(0) # just take him to the next round
+            question, answer = player.getQandA()
+            return render_template('base.html', time = time.time(), scores = Player.getScores(), count = "[ {0} / {1} ]".format(player.question+1, totalQuestions), username = username, success = False, question = question, score = player.score)
+    
+    return render_template('base.html', count = '[ 1 / {0} ]'.format(totalQuestions), time = time.time(),  scores = Player.getScores(), firstRound = True, username = username, question = question)
+    
+@app.route('/<username>', methods = ['GET', 'POST'])
+def user(username):
+    return render_template('base.html', time = time.time(),  username=username)
+    
+
+@app.route('/<username>/finished', methods = ['GET'])
+def finish(username):
+    player = users[username]
+    return render_template("finished.html", player = player, scores = Player.getScores())
+    
+if __name__ == '__main__':
+    app.run(host = os.getenv('IP'),
+            port = os.getenv('PORT'),
+            debug = True)
+
